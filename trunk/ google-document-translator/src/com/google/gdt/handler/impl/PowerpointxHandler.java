b0eb49761055ea2165947481ac2d50f32560c207
sanmoy@gmail.com <sanmoy@gmail.com@820a163b-2641-ae7e-4990-930cc72ba6ef>
2011-09-22 16:57:12 +0000

    pptx support

--- trunk/ google-document-translator/src/com/google/gdt/handler/impl/PowerpointxHandler.java
+++ trunk/ google-document-translator/src/com/google/gdt/handler/impl/PowerpointxHandler.java
@@ -20,27 +20,28 @@
 
 package com.google.gdt.handler.impl;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.OutputStream;
+import java.util.List;
+import java.util.logging.Level;
 import java.util.logging.Logger;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
+import java.util.zip.ZipOutputStream;
 
 import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
-import org.apache.poi.openxml4j.exceptions.OpenXML4JException;
-import org.apache.poi.xslf.XSLFSlideShow;
-import org.apache.poi.xslf.usermodel.XSLFSlide;
-import org.apache.xmlbeans.XmlException;
+import org.dom4j.Document;
+import org.dom4j.DocumentException;
+import org.dom4j.Node;
+import org.dom4j.io.SAXReader;
 
 import com.google.gdt.handler.DocumentHandler;
-import com.google.gdt.main.PreferenceModel;
 import com.google.gdt.ui.ProgressLevel;
-import com.google.gdt.util.AjaxTranslator;
-import com.google.gdt.util.HttpTranslator;
-import com.google.gdt.util.Translator;
-import com.google.gdt.util.TranslatorType;
 
 /**
  * 
@@ -51,6 +52,7 @@ public class PowerpointxHandler extends DocumentHandler
 {
 	private static Logger logger = Logger.getLogger("PowerpointxHandler.class");
 	
+	private final int BUFFER = 2048;
 	/**
 	 * 
 	 * @param inputFile
@@ -61,18 +63,121 @@ public class PowerpointxHandler extends DocumentHandler
 	@Override
 	public void handle(String inputFile,ProgressLevel pLevel) throws IOException, InvalidFormatException {
 		String outPutFile = getOuputFileName(inputFile);
-		OutputStream outputStream = new FileOutputStream(outPutFile);
-		InputStream inputStream = new FileInputStream(inputFile);
+		ZipInputStream zis = new ZipInputStream(new FileInputStream(inputFile));
+		ZipOutputStream zout = new ZipOutputStream(new FileOutputStream(outPutFile));
 		
-		try {
-			XSLFSlideShow xslfSlideShow = new XSLFSlideShow(outPutFile);
-		} catch (OpenXML4JException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (XmlException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+		int slideCount = getSlideCount(inputFile);
+		pLevel.setValue(0);
+		pLevel.setMaxValue(slideCount);
+		pLevel.setStringPainted(true);
+		pLevel.setTrFileName(outPutFile);
+		ZipEntry zipEntry;
+		int pBarUpdate = 0;
+		while((zipEntry=zis.getNextEntry())!=null)
+		{
+			if(zipEntry.getName().contains("ppt/slides/slide"))
+			{
+				ByteArrayOutputStream baos = new ByteArrayOutputStream();
+			    byte[] buffer = new byte[BUFFER];
+			    int len;
+			    while ((len = zis.read(buffer)) > 0 ) {
+			        baos.write(buffer, 0, len);
+			    }
+			    baos.flush();
+
+			    InputStream clone = new ByteArrayInputStream(baos.toByteArray()); 
+				SAXReader saxReader = new SAXReader();
+				Document doc = null;
+				try 
+				{
+					doc = saxReader.read(clone);
+					List<Node> nodes = doc.selectNodes("//p:sld/p:cSld/p:spTree/p:sp/p:txBody/a:p/a:r/a:t");
+					for(Node node : nodes)
+					{
+						if (isInterrupted) 
+						{
+							zis.close();
+							zout.close();
+							new File(outPutFile).delete();
+							pLevel.setString("cancelled");
+							return;
+						}
+						String inputText = "";
+						try
+						{
+							inputText = node.getText();
+							String translatedText = translator.translate(inputText);
+							node.setText(translatedText);
+						}
+						catch (Exception e) 
+						{
+							logger.log(Level.SEVERE, "Translation fails for the inputText : "+inputText, e);
+						}
+					}
+				} 
+				catch (DocumentException e) 
+				{
+					logger.log(Level.SEVERE, "cannot parse slide", e);
+				}
+				
+				zout.putNextEntry(new ZipEntry(zipEntry.getName()));
+				byte data[] = doc.asXML().getBytes("UTF8");
+				zout.write(data,0,data.length);
+				pBarUpdate++;
+				pLevel.setValue(pBarUpdate);
+			}
+			else
+			{
+				zout.putNextEntry(new ZipEntry(zipEntry.getName()));
+				int len;
+				byte data[] = new byte[BUFFER];
+				while((len=zis.read(data,0,BUFFER))!=-1)
+				{
+					zout.write(data,0,len);
+				}
+			}
 		}
+		zis.close();
+		zout.close();
+		pLevel.setValue(slideCount);
+		pLevel.setString("done");
+	}
+	
+	private int getSlideCount(String inputFile)
+	{
+		int count = 0;
+		ZipInputStream zis = null;
+		ZipEntry zipEntry;
+		try 
+		{
+			zis = new ZipInputStream(new FileInputStream(inputFile));
+			while((zipEntry=zis.getNextEntry())!=null)
+			{
+				if(zipEntry.getName().contains("ppt/slides/slide"))
+				{
+					count++;
+				}
+			}
+		} 
+		catch (IOException e) 
+		{
+			logger.log(Level.SEVERE, "cannot count the number of slides ", e);
+		}
+		finally
+		{
+			if(null!=zis)
+			{
+				try 
+				{
+					zis.close();
+				} 
+				catch (IOException e) 
+				{
+					e.printStackTrace();
+				}
+			}
+		}
+		return count;
 	}
 	
 }
