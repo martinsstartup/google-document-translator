505e32b0b92288b247394100a077f2e0e798db9b
sanmoy@gmail.com <sanmoy@gmail.com@820a163b-2641-ae7e-4990-930cc72ba6ef>
2012-01-04 17:56:44 +0000

    v1r8

--- trunk/ google-document-translator/src/com/google/gdt/ui/AboutBox.java
+++ trunk/ google-document-translator/src/com/google/gdt/ui/AboutBox.java
@@ -96,7 +96,7 @@ public class AboutBox extends javax.swing.JDialog {
         getContentPane().add(versionLabel);
         versionLabel.setBounds(165, 135, 92, 14);
 
-        appVersionLabel.setText("V1R7"); // NOI18N
+        appVersionLabel.setText("V1R8"); // NOI18N
         appVersionLabel.setName("appVersionLabel"); // NOI18N
         getContentPane().add(appVersionLabel);
         appVersionLabel.setBounds(263, 135, 36, 14);
