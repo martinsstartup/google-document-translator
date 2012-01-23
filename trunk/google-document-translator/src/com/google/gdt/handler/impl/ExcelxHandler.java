3956be86a2a7d17485e8ddb6869c6599c2ea0c8a
sanmoy@gmail.com <sanmoy@gmail.com@820a163b-2641-ae7e-4990-930cc72ba6ef>
2012-01-23 18:36:07 +0000

    change implementation of document handling

--- trunk/google-document-translator/src/com/google/gdt/handler/impl/ExcelxHandler.java
+++ trunk/google-document-translator/src/com/google/gdt/handler/impl/ExcelxHandler.java
@@ -20,20 +20,37 @@
 
 package com.google.gdt.handler.impl;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.OutputStream;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
+import java.util.zip.ZipOutputStream;
 
 import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
 import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.xssf.usermodel.XSSFRow;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.dom4j.Document;
+import org.dom4j.DocumentException;
+import org.dom4j.Node;
+import org.dom4j.io.SAXReader;
+import org.jaxen.JaxenException;
+import org.jaxen.SimpleNamespaceContext;
+import org.jaxen.XPath;
+import org.jaxen.dom4j.Dom4jXPath;
 
 import com.google.gdt.handler.DocumentHandler;
 import com.google.gdt.ui.ProgressLevel;
@@ -49,6 +66,8 @@ public class ExcelxHandler extends DocumentHandler
 
 	private static Logger logger = Logger.getLogger("ExcelxHandler.class");
 	
+	private final int BUFFER = 2048;
+	
 	/**
 	 * 
 	 * @param inputFile
@@ -59,58 +78,59 @@ public class ExcelxHandler extends DocumentHandler
 	public void handle(String inputFile,ProgressLevel pLevel) throws IOException, InvalidFormatException
 	{
 		String outPutFile = getOuputFileName(inputFile);
-		OutputStream outputStream = new FileOutputStream(outPutFile);
-		XSSFWorkbook xssfWorkbook = new XSSFWorkbook(inputFile);
-		List<XSSFSheet> sheets = getSheets(xssfWorkbook);
-		
-		pLevel.setTrFileName(outPutFile);
-		
-		for(int index=0; index<sheets.size();index++)
+		ZipInputStream zis = new ZipInputStream(new FileInputStream(inputFile));
+		ZipOutputStream zout = new ZipOutputStream(new FileOutputStream(
+				outPutFile));
+		ZipEntry zipEntry;
+		int wordCount = 0;
+		int pBarUpdate = 0;
+		while ((zipEntry = zis.getNextEntry()) != null) 
 		{
-			XSSFSheet xssfsheet = sheets.get(index);
-			
-			if(sheets.size()>1)
+			if (zipEntry.getName().equals("xl/sharedStrings.xml")) 
 			{
-				pLevel.setString("Translating sheet "+(index+1)+"/"+sheets.size());
-			}
-			
-			int firstRowNum = xssfsheet.getFirstRowNum();
-			int lastRowNum = xssfsheet.getLastRowNum();
-			int rowCount = lastRowNum - firstRowNum;
-			// check for empty sheet, don't perform any operation
-			if(rowCount==0)
-			{
-				continue;
-			}
-			pLevel.setValue(0);
-			pLevel.setMaxValue(rowCount);
-			pLevel.setStringPainted(true);
-			
-			int pBarUpdate = 0;
-			//iterate for rows
-			for ( int rowindex = firstRowNum; rowindex<=lastRowNum; rowindex++  )
-			{
-				XSSFRow xssfRow = xssfsheet.getRow(rowindex);
-				//iterate for cells
-				for ( Cell cell : xssfRow)
+				ByteArrayOutputStream baos = new ByteArrayOutputStream();
+				byte[] buffer = new byte[BUFFER];
+				int len;
+				while ((len = zis.read(buffer)) > 0) 
 				{
-					if(isInterrupted)
-					 {
-						 outputStream.close();
-						 new File(outPutFile).delete();
-						 pLevel.setString("cancelled");
-						 return;
-					 }
-					
-					 if(cell.getCellType()==Cell.CELL_TYPE_STRING)
-		             {
-						String inputText = cell.getStringCellValue();
-						String translatedTxt = inputText;
-						//in http post method, all key value pairs are seperated with &
-						if(preferenceModel.getTranslatorType()==TranslatorType.HTTP)
-							inputText = inputText.replaceAll("&", "and");
-						try
+					baos.write(buffer, 0, len);
+				}
+				baos.flush();
+
+				InputStream clone = new ByteArrayInputStream(baos.toByteArray());
+				SAXReader saxReader = new SAXReader();
+				Document doc = null;
+				Map<String,String> nameSpaceMap = new HashMap<String, String>(); 
+				nameSpaceMap.put("xmlns", "http://schemas.openxmlformats.org/spreadsheetml/2006/main");
+				try 
+				{
+					XPath xPath = new Dom4jXPath("//xmlns:t");
+					xPath.setNamespaceContext(new SimpleNamespaceContext(nameSpaceMap));
+					doc = saxReader.read(clone);
+					List<Node> nodes = xPath.selectNodes(doc);
+					wordCount = nodes.size();
+					pLevel.setValue(0);
+					pLevel.setMaxValue(wordCount);
+					pLevel.setStringPainted(true);
+					pLevel.setTrFileName(outPutFile);
+					pBarUpdate = 0;
+					for (Node node : nodes) 
+					{
+						pBarUpdate++;
+						pLevel.setValue(pBarUpdate);
+						if (isInterrupted) 
 						{
+							zis.close();
+							zout.close();
+							new File(outPutFile).delete();
+							pLevel.setString("cancelled");
+							return;
+						}
+						String inputText = "";
+						try 
+						{
+							inputText = node.getText();
+							String translatedTxt = inputText;
 							if(inputText.matches("\\s+"))//if the string is empty
 								continue;
 							if(inputText.contains("\n"))
@@ -123,41 +143,42 @@ public class ExcelxHandler extends DocumentHandler
 							{
 								translatedTxt = translator.translate(inputText);
 							}
-							cell.setCellValue(translatedTxt);
-						}
+							node.setText(translatedTxt);
+						} 
 						catch (Exception e) 
 						{
-							logger.log(Level.SEVERE, "Input File : "+inputFile+" cannot translate the text : "+inputText,e);
-							continue;
+							logger.log(Level.SEVERE,
+									"Translation fails for the inputText : "
+											+ inputText, e);
 						}
-		             }
+					}
+				} 
+				catch (DocumentException e)
+				{
+					logger.log(Level.SEVERE, "cannot parse slide", e);
+				} catch (JaxenException e) {
+					e.printStackTrace();
 				}
-				pBarUpdate++;
-				pLevel.setValue(pBarUpdate);
-			}
-			pLevel.setValue(rowCount);
-		}
-		
-		xssfWorkbook.write(outputStream);
-		outputStream.close();
-		
-		pLevel.setString("done");
-	}
 
-	/**
-	 * 
-	 * @param xssfWorkbook
-	 * @return
-	 */
-	private List<XSSFSheet> getSheets(XSSFWorkbook xssfWorkbook) 
-	{
-		List<XSSFSheet> xssfSheets = new ArrayList<XSSFSheet>();
-		for (int i = 0; i < xssfWorkbook.getNumberOfSheets(); i++) 
-		{
-			XSSFSheet xssfSheet = xssfWorkbook.getSheetAt(i);
-			xssfSheets.add(xssfSheet);
+				zout.putNextEntry(new ZipEntry(zipEntry.getName()));
+				byte data[] = doc.asXML().getBytes("UTF8");
+				zout.write(data, 0, data.length);
+			}
+			else 
+			{
+				zout.putNextEntry(new ZipEntry(zipEntry.getName()));
+				int len;
+				byte data[] = new byte[BUFFER];
+				while ((len = zis.read(data, 0, BUFFER)) != -1)
+				{
+					zout.write(data, 0, len);
+				}
+			}
 		}
-		return xssfSheets;
+		zis.close();
+		zout.close();
+		pLevel.setValue(wordCount);
+		pLevel.setString("done");
 	}
 
 }
